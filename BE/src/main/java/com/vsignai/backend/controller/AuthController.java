4e78cb1c9d60efe3f8c29c138325cad26123cf90
Phucdat25 <datnpse182125@fpt.edu.vn>
2026-05-10 12:18:11 +0700

    update profile

--- BE/src/main/java/com/vsignai/backend/controller/AuthController.java
+++ BE/src/main/java/com/vsignai/backend/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.vsignai.backend.controller;
 
+import com.vsignai.backend.dto.response.AuthResponse;
 import com.vsignai.backend.dto.LoginRequest;
 import com.vsignai.backend.dto.RegisterRequest;
+import com.vsignai.backend.entity.User;
 import com.vsignai.backend.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -16,17 +19,19 @@ public class AuthController {
 
 
     @PostMapping("/register")
-    public String register(@RequestBody RegisterRequest request) {
+    public AuthResponse register(@RequestBody RegisterRequest request) {
         return authService.register(request.getEmail(), request.getPassword(), request.getName());
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest request) {
+    public AuthResponse login(@RequestBody LoginRequest request) {
         return authService.login(request.getEmail(), request.getPassword());
     }
 
     @PostMapping("/google")
-    public String loginGoogle(@RequestBody Map<String, String> request) {
+    public AuthResponse loginGoogle(@RequestBody Map<String, String> request) {
         return authService.loginWithGoogle(request.get("idToken"));
     }
+
+
 }
--- BE/src/main/java/com/vsignai/backend/service/AuthService.java
+++ BE/src/main/java/com/vsignai/backend/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.vsignai.backend.service;
 
+import com.vsignai.backend.dto.response.AuthResponse;
+import com.vsignai.backend.entity.User;
+
 public interface AuthService {
-    public String register(String email, String password, String name);
-    public String login(String email, String password);
-    public String loginWithGoogle(String idToken);
+    public AuthResponse register(String email, String password, String name);
+    public AuthResponse login(String email, String password);
+    public AuthResponse loginWithGoogle(String idToken);
 }
