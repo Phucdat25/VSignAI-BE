b281b3e908332f0140816d30279cc68214286134
Phucdat25 <datnpse182125@fpt.edu.vn>
2026-05-28 01:35:09 +0700

    update 3

--- BE/src/main/java/com/vsignai/backend/service/ipml/VNPayServiceImpl.java
+++ BE/src/main/java/com/vsignai/backend/service/ipml/VNPayServiceImpl.java
@@ -87,7 +87,7 @@ public class VNPayServiceImpl implements VNPayService {
         String createDate = formatter.format(calendar.getTime());
         params.put("vnp_CreateDate", createDate);
 
-        calendar.add(Calendar.MINUTE, 15);
+        calendar.add(Calendar.MINUTE, 60);
 
         String expireDate =
                 formatter.format(calendar.getTime());
